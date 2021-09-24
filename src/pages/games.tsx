efb177aee7d2b08e2f41bcd30da823a7c4fbfbd0
Denner Luan <d.luancr@gmail.com>
2021-09-24 02:17:22 -0300

    Trabalhando com cache no Apollo

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,8 +1,9 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -11,23 +12,15 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+  await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
-    variables: {
-      limit: 9
-    }
+    variables: { limit: 15 }
   })
 
   return {
     props: {
       revalidate: 60,
-      games: data.games.map((game) => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover!.url}`,
-        price: game.price
-      })),
+      initialApolloState: apolloClient.cache.extract(),
       filterItems: filterItemsMock
     }
   }
