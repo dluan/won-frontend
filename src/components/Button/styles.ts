da39b4df0dec8be70f63ba440469f23cbb3dfcdf
Denner Luan <d.luancr@gmail.com>
2021-10-02 02:33:15 -0300

    Adicionadno Spinner

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -12,13 +12,6 @@ const wrapperModifiers = {
     height: 3rem;
     font-size: ${theme.font.sizes.xsmall};
   `,
-  minimal: (theme: DefaultTheme) => css`
-    background: none;
-    color: ${theme.colors.primary};
-    &:hover {
-      color: ${darken(0.1, theme.colors.primary)};
-    }
-  `,
   medium: (theme: DefaultTheme) => css`
     height: 4rem;
     font-size: ${theme.font.sizes.small};
@@ -35,9 +28,18 @@ const wrapperModifiers = {
   withIcon: (theme: DefaultTheme) => css`
     svg {
       width: 1.5rem;
+
       & + span {
         margin-left: ${theme.spacings.xxsmall};
       }
+    }
+  `,
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
     }
   `,
   disabled: () => css`
@@ -62,13 +64,18 @@ export const Wrapper = styled.button<WrapperProps>`
     padding: ${theme.spacings.xxsmall};
     text-decoration: none;
 
+    &:focus {
+      outline: 1px dashed;
+    }
+
     &:hover {
       background: ${minimal
         ? 'none'
         : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
+
     ${!!size && wrapperModifiers[size](theme)};
-    ${!!fullWidth && wrapperModifiers.fullWidth()}
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
     ${!!hasIcon && wrapperModifiers.withIcon(theme)};
     ${!!minimal && wrapperModifiers.minimal(theme)};
     ${disabled && wrapperModifiers.disabled()};
