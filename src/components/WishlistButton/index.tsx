9e76c8d76b55416fc03f98ed9e726f2076ddd224
Denner Luan <d.luancr@gmail.com>
2021-10-02 02:24:40 -0300

    Criando botão de wishlist

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -1,9 +1,47 @@
-import * as S from './styles'
+import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined'
+import Button, { ButtonProps } from 'components/Button'
+import { useWishlist } from 'hooks/use-wishlist'
+import { useSession } from 'next-auth/client'
 
-const WishlistButton = () => (
-  <S.Wrapper>
-    <h1>WishlistButton</h1>
-  </S.Wrapper>
-)
+type WishlistButtonProps = {
+  id: string
+  hasText?: boolean
+} & Pick<ButtonProps, 'size'>
+
+const WishlistButton = ({
+  id,
+  hasText,
+  size = 'small'
+}: WishlistButtonProps) => {
+  const [session] = useSession()
+  const { isInWishlist, addToWishlist, removeFromWishlist } = useWishlist()
+
+  const handleClick = () => {
+    isInWishlist(id) ? removeFromWishlist(id) : addToWishlist(id)
+  }
+
+  const ButtonText = isInWishlist(id)
+    ? 'Remove from Wishlist'
+    : 'Add to Wishlist'
+
+  if (!session) return null
+
+  return (
+    <Button
+      icon={
+        isInWishlist(id) ? (
+          <Favorite aria-label={ButtonText} />
+        ) : (
+          <FavoriteBorder aria-label={ButtonText} />
+        )
+      }
+      minimal
+      size={size}
+      onClick={handleClick}
+    >
+      {hasText && ButtonText}
+    </Button>
+  )
+}
 
 export default WishlistButton
--- src/components/WishlistButton/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import WishlistButton from '.'
-
-export default {
-  title: 'WishlistButton',
-  component: WishlistButton
-} as Meta
-
-export const Default: Story = () => <WishlistButton />
