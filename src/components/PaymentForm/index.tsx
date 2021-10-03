3e5ddb4ee1e993bae1606d009bb4fa3248021369
Denner Luan <d.luancr@gmail.com>
2021-10-03 02:19:30 -0300

    Salvando a ordem no banco de dados

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
 
 import { useCart } from 'hooks/use-cart'
@@ -9,7 +9,7 @@ import Button from 'components/Button'
 import Heading from 'components/Heading'
 
 import * as S from './styles'
-import { createPaymentIntent } from 'utils/stripe/methods'
+import { createPayment, createPaymentIntent } from 'utils/stripe/methods'
 import { Session } from 'next-auth/client'
 import { FormLoading } from 'components/Form'
 
@@ -66,6 +66,16 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     setError(event.error ? event.error.message : '')
   }
 
+  const saveOrder = async (paymentIntent?: PaymentIntent) => {
+    const data = await createPayment({
+      items,
+      paymentIntent,
+      token: session.jwt
+    })
+
+    return data
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
@@ -73,6 +83,9 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     // se for freeGames
     if (freeGames) {
       // salva no banco
+      // bater na API /orders
+      saveOrder()
+
       // redireciona para success
       push('/success')
       return
@@ -92,6 +105,9 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
       setLoading(false)
 
       // salvar a compra no banco do Strapi
+      // bater na API /orders
+      saveOrder(payload.paymentIntent)
+
       // redirectionar para a página de Sucesso
       push('/success')
     }
