151f021a0e4b4007973d3c7daebf0b1bc37fc747
Denner Luan <d.luancr@gmail.com>
2021-10-03 01:03:10 -0300

    Refatorando carrinho de compra

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -7,12 +7,10 @@ describe('<ProfileMenu />', () => {
   it('should render the menu', () => {
     const { container } = render(<ProfileMenu />)
 
-    // verificar os 4 links existentes
     expect(
       screen.getByRole('link', { name: /my profile/i })
     ).toBeInTheDocument()
 
-    expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: /sign out/i })
@@ -22,9 +20,9 @@ describe('<ProfileMenu />', () => {
   })
 
   it('should render the menu with an active link defined', () => {
-    render(<ProfileMenu activeLink="/profile/cards" />)
+    render(<ProfileMenu activeLink="/profile/orders" />)
 
-    expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+    expect(screen.getByRole('link', { name: /my orders/i })).toHaveStyle({
       background: theme.colors.primary,
       color: theme.colors.white
     })
